57aeef0c209345c832ffdc6eae914f6451cb81dc
Rem <rem1989487@gmail.com>
2025-10-01 13:23:29 +0500

    Final

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,28 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
+import java.lang.annotation.*;
 
 @Component
 public class AnimalsCage {
-
+    @Autowired
+    private Timer bean;
     @Autowired
     private Animal animal;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(bean.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return bean;
     }
 }
